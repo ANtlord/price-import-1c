54588b3ac36220b1cb6120a61f49a9c15571940e
antlord <antlord92@gmail.com>
2014-12-24 03:33:51 +0800

    little refactoring

--- src/SaveCommand.cpp
+++ src/SaveCommand.cpp
@@ -8,23 +8,20 @@
 
 using pqxx::work;
 using std::list;
-SaveCommand::SaveCommand(const char* table, std::string fields[], size_t n)
+SaveCommand::SaveCommand(const char* table, std::string fields[], size_t n,
+        const char * key) : _KEY(key), _TABLE(table), _N(n)
 {
-    _fields = fields;
-    _n = n;
-    _table = std::string(table);
+    _FIELDS = fields;
 }
-
-SaveCommand::SaveCommand(){}
 
 SaveCommand::~SaveCommand()
 {
-    delete[] _fields;
+    delete[] _FIELDS;
 }
 
-std::string * SaveCommand::getFields() const
+const std::string * SaveCommand::getFields() const
 {
-    return _fields;
+    return _FIELDS;
 }
 
 
@@ -34,43 +31,44 @@ bool SaveCommand::execute() const
     else {
         work w(*(DBSingleton::getSingleton()->getConnection()));
         list<std::string*> updateData;
-        const std::string UNIQUE_FIELD = "name";
         const size_t IDX = 0;
 
-        std::string insertQueryString = "insert into "+_table+" ";
+        std::string insertQueryString = "insert into "+_TABLE+" ";
 
-        for (auto it = _data.begin(); it != _data.end(); ++it) {
-            std::string query = "select exists(select 1 from "+_table
-                    +" where "+UNIQUE_FIELD+" ilike "+w.quote(*it[IDX])+")";
-            pqxx::result res;
-            
-            try {
-                res = w.exec(query);
-            }
-            catch (pqxx::data_exception &e) {
-                std::cout << "Error query: " << query << std::endl;
-                return false;
-            }
-            if (res.size() != 1) {
-                std::cout << "Error while checking existance of "<< it[IDX] << std::endl;
-                return false;
-            }
-            else if (res[0][0].as<bool>()) {    // update entry.
-                updateData.push_back(*it);
-            }
-            else {  // insert entry.
+        if (_KEY != 0) {
+            for (auto it = _data.begin(); it != _data.end(); ++it) {
+                std::string query = "select exists(select 1 from "+_TABLE
+                        +" where "+_KEY+" ilike "+w.quote(*it[IDX])+")";
+                pqxx::result res;
+                
+                try {
+                    res = w.exec(query);
+                }
+                catch (pqxx::data_exception &e) {
+                    std::cout << "Error query: " << query << std::endl;
+                    return false;
+                }
+                if (res.size() != 1) {
+                    std::cout << "Error while checking existance of "<< it[IDX] << std::endl;
+                    return false;
+                }
+                else if (res[0][0].as<bool>()) {    // update entry.
+                    updateData.push_back(*it);
+                }
+                else {  // insert entry.
 
+                }
             }
         }
 
-        std::string queryString = "insert into "+_table+" ";
-        queryString += ("("+forge::join(_fields, _n, ", ")+") values ");
+        std::string queryString = "insert into "+_TABLE+" ";
+        queryString += ("("+forge::join(_FIELDS, _N, ", ")+") values ");
 
         size_t i = 0;
         for (auto it = _data.begin(); it != _data.end(); ++it) {
             forge::each<std::string>([&w](std::string &item){
-                    item = w.quote(item);}, *it, _n);
-            queryString += ("("+forge::join(*it, _n, ", ")+")");
+                    item = w.quote(item);}, *it, _N);
+            queryString += ("("+forge::join(*it, _N, ", ")+")");
             if (i < _data.size()-1) {
                 queryString += ",";
             }
@@ -89,7 +87,7 @@ bool SaveCommand::execute() const
 
 size_t SaveCommand::getFieldsLength() const
 {
-    return _n;
+    return _N;
 }
 
 void SaveCommand::clearData()
