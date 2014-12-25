2fe851a92e9dc284d344f36a2db9040ea5f033ab
antlord <antlord92@gmail.com>
2014-12-26 03:51:21 +0800

    updating of products has been added

--- src/SaveCommand.cpp
+++ src/SaveCommand.cpp
@@ -44,38 +44,33 @@ bool SaveCommand::execute() const
         std::string insertQueryString = "insert into "+_TABLE+" ";
 
         if (_KEY != 0) {
-            size_t i = 0;
             for (auto it = _data.begin(); it != _data.end(); ++it) {
-                std::cout << "before query" << i << std::endl;
                 std::string query = "select exists(select 1 from "+_TABLE
                         +" where "+_KEY+" = "+w.quote((*it)[_keyIndex])+")";
                 pqxx::result res;
                 
-                std::cout << "before try" << i << std::endl;
                 try {
                     res = w.exec(query);
                 }
                 catch (pqxx::data_exception &e) {
-                    std::cout << "in catch" << i << std::endl;
                     std::cout << "Error query: " << query << std::endl;
                     return false;
                 }
 
-                std::cout << "before checking res size" << i << std::endl;
                 if (res.size() != 1) {
-                    std::cout << "Error while checking existance of "<< (*it)[_keyIndex] << std::endl;
+                    std::cout << "Error while checking existance of "
+                        << (*it)[_keyIndex] << std::endl;
                     return false;
                 }
                 else if (res[0][0].as<bool>()) {    // update entry.
                     //updateData.push_back(*it);
-                    std::cout << "before building update query" << i << std::endl;
                     std::string arr[_N-1];
                     for (size_t i = 1; i < _N; ++i) {
                         arr[i-1] = _FIELDS[i] + "=" + w.quote((*it)[i]);
                     }
                     std::string pars = forge::join(arr, (_N-1), ", ");
-                    std::cout << "before executing update query" << i << std::endl;
-                    w.exec("update "+_TABLE+" set "+pars+" where "+_KEY+"="+w.quote((*it)[_keyIndex]));
+                    w.exec("update "+_TABLE+" set "+pars+" where "+_KEY+"="
+                            +w.quote((*it)[_keyIndex]));
                 }
                 else if (!res[0][0].as<bool>()) {    // insert entry.
                     insertData.push_back(*it);
@@ -83,7 +78,6 @@ bool SaveCommand::execute() const
                 else { // Something just is wrong over there. 
                      //TODO: handle exception.
                 }
-                ++i;
             }
         }
 
@@ -91,23 +85,26 @@ bool SaveCommand::execute() const
         queryString += ("("+forge::join(_FIELDS, _N, ", ")+") values ");
 
         size_t i = 0;
-        if (insertData.size() == 0 && updateData.size() == 0) {
+        //if (insertData.size() == 0 && updateData.size() == 0) {
+        if (_KEY == 0) {
             insertData = _data;
         }
-        for (auto it = insertData.begin(); it != insertData.end(); ++it) {
-            forge::each<std::string>([&w](std::string &item){
-                    item = w.quote(item);}, *it, _N);
-            queryString += ("("+forge::join(*it, _N, ", ")+")");
-            if (i < _data.size()-1) {
-                queryString += ",";
+        if (insertData.size() > 0) {
+            for (auto it = insertData.begin(); it != insertData.end(); ++it) {
+                forge::each<std::string>([&w](std::string &item){
+                        item = w.quote(item);}, *it, _N);
+                queryString += ("("+forge::join(*it, _N, ", ")+")");
+                if (i < _data.size()-1) {
+                    queryString += ",";
+                }
+                ++i;
             }
-            ++i;
-        }
-        try {
-            w.exec(queryString);
-        }
-        catch (pqxx::data_exception &e) {
-            std::cout << "Error query: " << queryString << std::endl;
+            try {
+                w.exec(queryString);
+            }
+            catch (pqxx::data_exception &e) {
+                std::cout << "Error query: " << queryString << std::endl;
+            }
         }
         w.commit();
         return true;
