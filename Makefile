ae0d941ddfc7067c1ec073f74c1b4ab484efa928
antlord <antlord92@gmail.com>
2015-01-15 16:01:12 +0800

    test code of libxls has been created

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 CC=g++ -std=c++0x
 #CXXFLAGS=$(shell wx-config --cxxflags)
 #LIBS=$(shell wx-config --gl-libs --libs)
-VENDOR_LIBS=-L./vendor/forge -lforge -lpqxx -L./vendor/libxls/lib/libxlsreader.a
+VENDOR_LIBS=-L./vendor/forge -lforge -lpqxx -L./vendor/libxls/lib/ -lxlsreader
 VENDOR_HEADERS=-I./vendor/forge/include -I./vendor/libxls/include
 
 #AL_LIBS=-lalut -lvorbisfile -logg
--- src/ExcelReader.cpp
+++ src/ExcelReader.cpp
@@ -1,5 +1,6 @@
 #include "../include/ExcelReader.h"
-#include <libxls/xls.h>
+#include <xls.h>
+#include <string.h>
 using std::string;
 ExcelReader::ExcelReader(std::string filepath) : DataFileReader(filepath)
 {
@@ -8,6 +9,11 @@ ExcelReader::ExcelReader(std::string filepath) : DataFileReader(filepath)
 
 bool ExcelReader::readline(std::string &buffer)
 {
+    xls::xlsWorkBook* pWB;
+    xls::xlsWorkSheet* pWS;
+    string encoding = "utf-8";
+    pWB=xls::xls_open(_filepath.c_str(), encoding.c_str());
+ 
     return false;
 }
 
